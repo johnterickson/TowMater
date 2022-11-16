126bff53478ca34a1712c92478687d416a5f5d3f
John Erickson <john.t.erickson@gmail.com>
2022-11-15 20:21:33 -0800

    some improvements

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{error::Error, convert::TryInto};
 
 mod angle;
 use angle::Angle;
-use na::{Vector3, Vector2};
+use na::Vector3;
 use rlbot::RLBot;
 
 const BUILD_TIME : &str = include!(concat!(env!("OUT_DIR"), "/timestamp.txt"));
@@ -57,7 +57,7 @@ struct MyBot {
 
 impl MyBot {
     const HEADER_1_GROUP_ID: i32 = 1000;
-    const CAR_GROUP_ID: i32 = 1001;
+    const CAR_GROUP_BASE_ID: i32 = 1010;
 
     const BALL_RADIUS: f32 = 90.0;
 
@@ -79,6 +79,13 @@ impl MyBot {
 
         let car = &packet.players[self.player_index];
         let car_pos = car.physics.location.to_vec3();
+        let (car_pitch, car_yaw, car_roll) = {
+            let angles = &car.physics.rotation;
+            (Angle::from_radians(angles.pitch), Angle::from_radians(angles.yaw), Angle::from_radians(angles.roll))
+        };
+
+        let opp = &packet.players[1 - self.player_index];
+        let opp_pos = opp.physics.location.to_vec3();
 
         let ball = packet.ball.as_ref().ok_or("ball not found")?;
         let ball_pos = ball.physics.location.to_vec3();
@@ -98,20 +105,29 @@ impl MyBot {
         let my_goal_pos = self.my_goal.ok_or("cannot find my goal")?;
         let opp_goal_pos = self.opp_goal.ok_or("cannot find opp goal")?;
 
-        let distance_to_my_goal = (car_pos - my_goal_pos).magnitude();
-        let distance_to_opp_goal = (car_pos - opp_goal_pos).magnitude();
+        let ball_to_my_goal = my_goal_pos - ball_pos;
+        let ball_to_opp_goal = opp_goal_pos - ball_pos;
+        let ball_closer_to_opp_goal = ball_to_my_goal.magnitude() > ball_to_opp_goal.magnitude();
 
-        let mut group = rlbot.begin_render_group(Self::CAR_GROUP_ID);
+        let car_to_opp_goal = opp_goal_pos - car_pos;
+        let opp_to_opp_goal = opp_goal_pos - opp_pos;
+        let breakaway = 
+            ball_to_opp_goal.magnitude() < car_to_opp_goal.magnitude() &&
+            car_to_opp_goal.magnitude() < opp_to_opp_goal.magnitude();
+
+        let mut group = rlbot.begin_render_group(Self::CAR_GROUP_BASE_ID + self.player_index as i32);
         let white = group.color_rgb(255, 255, 255);
         let red = group.color_rgb(255, 0, 0);
 
-        let target_pos = if distance_to_my_goal > distance_to_opp_goal {
-            group.draw_string_3d((car_pos.x, car_pos.y, car_pos.z), (1,1), "OFFENSE!", white);
-            let mut opp_goal_to_ball_dir = (ball_pos - opp_goal_pos).normalize();
-            // exaggerate x to make it nudge more
-            opp_goal_to_ball_dir[0] *= 1.5;
-            let opp_goal_to_ball_dir = opp_goal_to_ball_dir.normalize();
-            ball_pos + Self::BALL_RADIUS * 0.9 * opp_goal_to_ball_dir            
+        
+        let offense = ball_closer_to_opp_goal || breakaway;
+
+        let target_pos = if offense {
+            group.draw_string_3d((car_pos.x, car_pos.y, car_pos.z), (1,1), if breakaway { "BREAKAWAY!" } else { "OFFENSE!" }, white);
+            let ball_to_opp_goal_dir = ball_to_opp_goal.normalize();
+            let ball_vel_dir = ball_vel.normalize();
+            let nudge_dir = (ball_to_opp_goal_dir - ball_vel_dir).normalize();
+            ball_pos + Self::BALL_RADIUS * -1.1 * nudge_dir            
         } else {
             group.draw_string_3d((car_pos.x, car_pos.y, car_pos.z), (1,1), "DEFENSE!", white);
             let ball_to_my_goal_dir = (my_goal_pos - ball_pos).normalize();
@@ -128,23 +144,25 @@ impl MyBot {
         
         let car_to_target = target_pos - car_pos;
         let car_to_ball = ball_pos - car_pos;
-        let target_pos = if car_to_target.magnitude() > car_to_ball.magnitude() && ball_pos[2] < 2.0 * Self::BALL_RADIUS {
+        let target_on_far_side_of_ball = car_to_target.magnitude() > car_to_ball.magnitude();
+        let can_sneak_under_ball = car_to_target.magnitude() < ball_pos[2];
+        let target_pos = if target_on_far_side_of_ball && !can_sneak_under_ball {
             group.draw_line_3d((car_pos.x, car_pos.y, car_pos.z), (target_pos.x, target_pos.y, target_pos.z), red);
             // we are on the wrong side of the ball
             let ball_to_target = car_to_target - car_to_ball;
 
             group.draw_line_3d((ball_pos.x, ball_pos.y, ball_pos.z), (target_pos.x, target_pos.y, target_pos.z), white);
 
-            // a is the amount of ball_to_target parallel with car_to_ball
-            let a = ball_to_target.dot(&car_to_ball) / car_to_ball.magnitude();
-            let ball_to_target_parallel = a * car_to_ball.normalize();
-            // B is a vector of ball_to_target that is not parallel to car_to_ball
-            let ball_to_target_perpindicular = ball_to_target - ball_to_target_parallel;
-            if ball_to_target_perpindicular.magnitude() > 2.0 * Self::BALL_RADIUS {
+            // split ball_to_target into the part parallel to car_to_ball and the rest
+            let (_ball_to_target_parallel, ball_to_target_rest) = 
+                rebasis(&car_to_ball, &ball_to_target);
+
+            // do we already have sufficient clearance?
+            if ball_to_target_rest.magnitude() > 2.0 * Self::BALL_RADIUS {
                 target_pos
             } else {
                 group.draw_string_3d((car_pos.x, car_pos.y, car_pos.z), (1,1), "GO AROUND!", white);
-                target_pos + 2.0 * Self::BALL_RADIUS * ball_to_target_perpindicular.normalize()
+                target_pos + 1.5 * Self::BALL_RADIUS * ball_to_target_rest.normalize()
             }
         } else {
             target_pos
@@ -176,16 +194,25 @@ impl MyBot {
 
         let mut jump = false;
 
+        // go for a header
         if car_to_target_xy_distance < Self::BALL_RADIUS {
             // if above me and coming down
             if ball_pos[2] - car_pos[2] > 1.0 * Self::BALL_RADIUS  && ball_vel[2] < 0.0 {
                 let ball_land_time = ball_pos[2] / (-1.0 * ball_vel[2]);
                 group.draw_string_3d((car_pos.x, car_pos.y, car_pos.z + 50.0), (1,1), format!("ball_land_time: {}", ball_land_time), white);
 
-                if ball_land_time < 0.5 {
+                if ball_land_time < 0.8 {
                     jump = true;
                 }
             }
+        }
+
+        // stuck on wall? jump off it and level
+        if   car_pos[2] > 2.0 * Self::BALL_RADIUS && 
+            (car_pitch.degrees() > 70.0 || car_roll.degrees() > 70.0) &&
+            car.has_wheel_contact
+        {
+            jump = true;
         }
 
         group.render()?;
@@ -198,6 +225,52 @@ impl MyBot {
         })
     }
 }
+
+
+// projection of b onto a
+fn project(b: &Vector3<f32>, a: &Vector3<f32>) -> Vector3<f32> {
+    let a_len = a.magnitude();
+    a.dot(b) / (a_len * a_len) * a
+}
+
+fn rebasis(basis: &Vector3<f32>, v: &Vector3<f32>) -> (Vector3<f32>, Vector3<f32>) {
+    let parallel = project(v, basis);
+    let rest = v - parallel;
+    (parallel, rest)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn project_tests() {
+        let b = Vector3::new(1.0, 1.0, 0.0);
+        let a = Vector3::new(1.0, 0.0, 0.0);
+        let actual = project(&b, &a);
+        let expected = Vector3::new(1.0, 0.0, 0.0);
+        assert_approx_eq_vec(actual, expected);
+    }
+
+    #[test]
+    fn rebasis_tests() {
+        let basis = Vector3::new(1.0, 1.0, 0.0);
+        let v = Vector3::new(1.0, 0.0, 0.0);
+        let (parallel, rest) = 
+        rebasis(&basis, &v);
+        assert_approx_eq_vec(parallel + rest, v);
+        assert_approx_eq(parallel.dot(&basis), 1.0);
+    }
+
+    fn assert_approx_eq_vec(a: Vector3<f32>, b: Vector3<f32>) {
+        assert!((a - b).magnitude() < 0.01, "{} vs {}", a, b);
+    }
+
+    fn assert_approx_eq(a: f32, b: f32) {
+        assert!((a - b).abs() < 0.01, "{} vs {}", a, b);
+    }
+}
+
 
 trait ToVec3 {
     fn to_vec3(&self) -> na::Vector3<f32>;
